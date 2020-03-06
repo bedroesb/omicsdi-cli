82ae178ff192330eaa8dcac606702947384271b1
bedro <bedro@psb.ugent.be>
2020-03-06 11:46:31 +0100

    using click

--- omicsdi_data.py
+++ omicsdi_data.py
@@ -1,30 +1,33 @@
-import argparse
-from sys import argv
+import click
 from omics_client import OmcicsClient
 
-acc_number = argv[1]
 client = OmcicsClient()
+
 
 def source_from_id(acc):
     api_output = client.get_source(acc)
     source = api_output['datasets'][0]['source']
     return source
 
+
 def ftp_links(source, acc):
     api_output = client.get_data(source, acc)
     ftp = []
     files = api_output['file_versions'][0]['files']
-    
-    for ext,file_links in files.items():
+
+    for ext, file_links in files.items():
         for file_link in file_links:
             ftp.append(file_link)
     return ftp
 
-
-def ftp_from_id(acc):
+@click.command()
+@click.argument('acc')
+def main(acc):
     source = source_from_id(acc)
     ftp = ftp_links(source, acc)
-    return ftp
+    output = '\n'.join(ftp)
+    print(output)
 
 
-print('\n'.join(ftp_from_id(acc_number)))
+if __name__ == "__main__":
+    main()
